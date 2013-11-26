e78845b2816057fdfbd7cac4a3940402f55afb5f
José Pablo Sánchez Cano <pabletesc@gmail.com>
2013-11-26 19:42:02 +0100

    subiendo después de un pulo completo

---
+++ ifinder/CelldePuntos.h
@@ -0,0 +1,13 @@
+//
+//  CelldePuntos.h
+//  ifinder
+//
+//  Created by pablete on 26/11/13.
+//  Copyright (c) 2013 German Bonilla Monterde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CelldePuntos : UITableViewCell
+
+@end
